1281fd0c137cf3dd5cd54b8d702b93a0273a0381
Bulgan Batmagnai <bulgan@Bulgans-MacBook-Pro.local>
2016-12-05 14:55:26 -0800

    changes

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {IndexRoute, Route, browserHistory } from 'react-router';
+import ReactStormpath, { Router, HomeRoute, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
+
+ReactStormpath.init();
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path='/' component={MasterPage}>
+     <IndexRoute component={IndexPage} />
+    </Route>
+  </Router>,
+  document.getElementById('app-container')
+);
